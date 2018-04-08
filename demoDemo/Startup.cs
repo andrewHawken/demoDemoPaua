ba98883168dc57340f26f6b18b2ca1724b47383c
andrewHawken <ajhawken@gmail.com>
2018-04-08 19:23:48 +1200

    update

--- demoDemo/Startup.cs
+++ demoDemo/Startup.cs
@@ -30,7 +30,7 @@ namespace demoDemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World2!");
             });
         }
     }
